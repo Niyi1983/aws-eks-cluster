23c581ea145132268395d635e0d9a6668abf832d
Olagundoye T. Niyi <teejaybloom@yahoo.com>
2025-01-11 08:50:46 -0800

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,20 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+data "aws_availability_zones" "available" {}
+
+# Not required: currently used in conjunction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
